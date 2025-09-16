3cd0711559ffd384a82d3d75eb3f548020282d9c
hdbenosx10 <benosx03@gmail.com>
2025-09-16 03:03:13 -0300

    experiencias

--- src/components/Experiencias.jsx
+++ src/components/Experiencias.jsx
@@ -1,4 +1,79 @@
+// src/components/Experiencias.jsx
 import React from 'react';
+import styled from 'styled-components';
 
-const Experiencias = () => <div>Experiências</div>;
+const Section = styled.section`
+  margin-bottom: 60px;
+`;
+
+const ItemGrid = styled.div`
+  display: grid;
+  grid-template-columns: 1fr;
+  gap: 24px;
+
+  @media(min-width: ${({ theme }) => theme.breakpoints.tablet}) {
+    grid-template-columns: repeat(2, 1fr);
+  }
+`;
+
+const Item = styled.div`
+  display: flex;
+  flex-direction: column;
+  gap: 12px;
+`;
+
+const ItemImg = styled.img`
+  border-radius: 8px;
+  width: 100%;
+  height: auto;
+`;
+
+const ItemText = styled.p`
+  color: ${({ theme }) => theme.colors.primary};
+  font-size: 1rem;
+`;
+
+const Experiencias = () => {
+  const items = [
+    {
+      empresa: 'Globo',
+      texto: 'Em 2021, atuei nos jornais mais importantes ... etc',
+      imgSrc: '/images/globo1.jpg' // etc
+    },
+    {
+      empresa: 'Globo',
+      texto: 'Em 2023, recebi uma menção honrosa ...',
+      imgSrc: '/images/globo2.jpg'
+    },
+    {
+      empresa: 'Globo',
+      texto: 'Em 2025, talvez o maior momento da minha carreira ...',
+      imgSrc: '/images/globo3.jpg'
+    },
+    {
+      empresa: 'ESPN',
+      texto: 'Em 2020, eu mudei para São Paulo ...',
+      imgSrc: '/images/espn.jpg'
+    },
+    {
+      empresa: 'FOX Sports',
+      texto: 'Em 2017, comecei minha carreira ...',
+      imgSrc: '/images/foxsports.jpg'
+    },
+  ];
+
+  return (
+    <Section>
+      <ItemGrid>
+        {items.map((item, idx) => (
+          <Item key={idx}>
+            <ItemImg src={item.imgSrc} alt={`Experiência ${item.empresa}`} />
+            <ItemText>{item.texto}</ItemText>
+          </Item>
+        ))}
+      </ItemGrid>
+    </Section>
+  );
+};
+
 export default Experiencias;
